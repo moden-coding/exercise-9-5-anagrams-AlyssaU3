97de764cb129486dd32eb25cfefa4cf96659e590
AlyssaU3 <alys4321c@gmail.com>
2025-09-02 20:56:10 -0400

    exersise 9.5 done

--- src/anagrams.py
+++ src/anagrams.py
@@ -1,6 +1,11 @@
 # Write your solution here
 def anagrams(word1 : str, word2 : str):
-    pass
+    a= sorted(word1)
+    b= sorted(word2)
+    check= False
+    if a==b:
+        check=True
+    return check
 
 if __name__ == "__main__":
     print(anagrams("tame", "meta")) # True
